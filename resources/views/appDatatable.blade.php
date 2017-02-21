0d4dcbbde2192fe2043c146f0eca030143333558
mick byrne <mick.byrne@Churchill1795.com>
2017-02-21 10:54:45 +0000

    Assets now versioned properly.

--- resources/views/appDatatable.blade.php
+++ resources/views/appDatatable.blade.php
@@ -7,7 +7,7 @@
 	<title>IT Utils</title>
 
 
-	<link rel="stylesheet" href="{{ elixir('/css/all.css') }}">
+	<link rel="stylesheet" href="{{ elixir('css/all.css') }}">
 
 	<link rel="shortcut icon" href="/favicon.ico" >
 
@@ -37,8 +37,8 @@
 
 
 
-<script src="{{ elixir('/js/all.js') }}"></script>
-<script src="{{ elixir('/js/app.js') }}"></script>
+<script src="{{ elixir('js/all.js') }}"></script>
+<script src="{{ elixir('js/app.js') }}"></script>
 @yield('js')
 
 
